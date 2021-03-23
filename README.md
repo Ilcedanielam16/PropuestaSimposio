d128a66a2dcc8276ef1d9bbde2b1c734369f1415
Ilcedanielam16 <78665970+Ilcedanielam16@users.noreply.github.com>
2021-03-23 10:10:45 -0600

    Rama
    Creacion de rama

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # PropuestaSimposio
  Evaluación parcial de introducción a la ingeniería en informática y sistemas
+
+## Lenguaje que se utilizo 
+Este **programa** se realizo con **C++**
+
+# Programa indicaciones 
+Programa que pregunte por los nombres de los 2 conferencistas y sus respectivas edades, luego el programa debe mostrar en pantalla que conferencista es el más joven. 
+
+# Enlace
+[GitHub Pages](https://github.com/Ilcedanielam16/PropuestaSimposio).
+
+# Lista Caracteres
+- Nombres
+- Edades 
+
+# Utilizacion de ciclo if 
+Calculo de edades 
--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
+// Ilce Daniela Morales Arreaga
 #include <iostream>
+using namespace std;
 int main()
 {
-  std::cout << "Propuesta de programa para el simposio de Ingenieria 2021\n";
-  std::cin.ignore();
+  char nombre1[15]; 
+  char nombre2[15];
+  int edad1;
+  int edad2;
+  cout << "Ingrese el nombre del primer conferencista\n";
+  cin>> nombre1;
+  cout <<"Ingrese su edad \n";
+  cin >> edad1;
+  cout <<"Ingrese el nombre del segundo conferencista\n";
+  cin>> nombre2;
+  cout <<"Ingrese su edad \n";
+  cin >> edad2;
+  if (edad1>=edad2){
+    cout<< nombre1 << "Es el mayor" << edad1 <<endl;  
+  }
+  else {
+    cout<< nombre2 << "Es el mayor"<< edad2 <<endl;
+  }
+  cin.ignore();
   return 0;
 }
